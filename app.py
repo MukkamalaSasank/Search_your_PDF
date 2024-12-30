3ed961b5b54d126952b481e91293198a81060f13
SASANK MUKKAMALA <sasankmukkamala2303@gmail.com>
2024-12-29 20:48:44 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,83 +1,3 @@
-# import streamlit as st
-# from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-# from transformers import pipeline
-# import torch
-# from langchain_community.embeddings import SentenceTransformerEmbeddings
-# # from langchain_community.vectorstores import Chroma
-# from langchain_chroma import Chroma
-# from langchain_community.llms import HuggingFacePipeline
-# from langchain.chains import RetrievalQA
-
-# # Define the checkpoint
-# checkpoint = "LaMini-T5-738M"
-
-# # Load the tokenizer
-# tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-
-# # Load the model directly onto the CPU or GPU
-# device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
-# # Load the model
-# base_model = AutoModelForSeq2SeqLM.from_pretrained(
-#     checkpoint, 
-#     torch_dtype=torch.float32
-# )
-
-# # Move the model to the desired device
-# base_model.to(device)
-
-# @st.cache_resource
-# def llm_pipeline():
-#     pipe = pipeline(
-#         'text2text-generation',
-#         model=base_model,
-#         tokenizer=tokenizer,
-#         max_length=256,
-#         do_sample=True,
-#         temperature=0.3,
-#         top_p=0.95
-#     )
-#     local_llm = HuggingFacePipeline(pipeline=pipe)
-#     return local_llm
-
-# @st.cache_resource
-# def qa_llm():
-#     llm = llm_pipeline()
-#     embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
-#     db = Chroma(persist_directory="db", embedding_function=embeddings)
-#     retriever = db.as_retriever()
-#     qa = RetrievalQA.from_chain_type(
-#         llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True)
-#     return qa
-
-# def process_answer(instruction):
-#     qa = qa_llm()
-#     generated_text = qa(instruction)
-#     answer = generated_text['result']
-#     return answer, generated_text
-
-# def main():
-#     st.title("Search Your PDF 🐦📄")
-#     with st.expander("About the App"):
-#         st.markdown(
-#             """
-#             This is a Generative AI powered Question and Answering app that responds to questions about your PDF File.
-#             """
-#         )
-#     question = st.text_area("Enter your Question")
-#     if st.button("Ask"):
-#         st.info("Your Question: " + question)
-
-#         st.info("Your Answer")
-#         answer, metadata = process_answer(question)
-#         st.write(answer)
-#         st.write(metadata)
-
-# if __name__ == '__main__':
-#     main()
-
-
-
 import streamlit as st
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 from transformers import pipeline
@@ -111,7 +31,7 @@ def llm_pipeline():
         'text2text-generation',
         model=base_model,
         tokenizer=tokenizer,
-        max_length=512,
+        max_length=1024,
         do_sample=True,
         temperature=0.5,
         top_p=0.9
